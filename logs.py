573d5fd425b8766640ac33b2b51f36e34d54d763
capgelka <ldyach@yandex.ru>
2016-09-18 21:36:28 +0300

    bug fix and refactoring

--- logs.py
+++ logs.py
@@ -9,72 +9,28 @@ import re
 T = TypeVar('T')
 
 
-# class RingBuffer(MutableSequence):
-
-#     def __init__(self, input_stream: Iterable[T] = [], bounds: Union[int, None] = None) -> None:
-#         if bounds is None:
-#             self._data = list(input_stream)
-#         else:
-#             self._data = []
-#             for num, elem in enumerate(input_stream):
-#                 self._data.append(elem)
-#                 if num == bounds - 2:
-#                     break
-#         self.size = len(self._data)
-#         self._start = 0
-
-#     def __shifted(self, value: int) -> int:
-#         return (self._start + value) % self.size
-
-#     def __getitem__(self, num: int) -> T:
-#         # print(self._data[self.__shifted(num)])
-#         # print(self._start, num, self.size)
-#         return self._data[self.__shifted(num)]
-
-#     def insert(self, index: int, value: T) -> None:
-#         self._data.insert(self.__shifted(index), value)
-#         self.size += 1
-
-#     def __delitem__(self, index: int) -> None:
-#         del self._data[self.__shifted(index)]
-#         self.size -= 1
-
-#     def __len__(self) -> int:
-#         return self.size
-
-#     def __setitem__(self, key: int, value: T) -> None:
-#         self._data[self.__shifted(key)] = value
-
-#     def add_last(self, value: T) -> None:
-#         self._data[self._start] = value
-#         self._start += 1
-
-#     def add_first(self, value: T) -> None:
-#         self[self.size - 1] = value
-#         print('call')
-#         self._start = self._start - 1 if self._start > 0 else self.size
-
-
 class LogFrame(object):
 
     def __init__(self, left_size: int, right_size: int, stream: Iterable[str]) -> None:
-        self.left = deque(islice(stream, left_size), left_size)
-        self.curr = next(iter(stream))
-        self.right = deque(islice(stream, right_size), right_size)
+        size =  left_size + right_size + 1
+        self.buff = deque(islice(stream, size), size)
+        self._current = left_size
+        # self.left = deque(islice(stream, left_size), left_size)
+        # self.curr = next(iter(stream))
+        # self.right = deque(islice(stream, right_size), right_size)
 
     def show(self) -> None:
-        print(*list(self.left),
-              self.curr,
-              *list(self.right),
-              sep='\n')
+        print(*self.buff)
 
     def shift(self, value: str) -> None:
-        self.right.appendleft(self.curr)
-        self.curr = self.left[-1]
-        self.left.appendleft(value)
+        self.buff.append(value)
+
+    @property
+    def current(self):
+        return self.buff[self._current]
 
     def __iter__(self):
-        return chain(self.left, (self.curr,), self.right)
+        return iter(self.buff)
 
 
 
@@ -103,7 +59,7 @@ class LogFrameSearchStream(LogFrameStream):
                  predicate: Callable[[str], bool],
                  left_size: int = 100, 
                  right_size: int = 100) -> None:
-        print(type(logstream))
+        # print(type(logstream))
         first = list(islice(logstream, left_size + right_size + 1))
         found = [(n, x) for (n, x) in enumerate(first) if predicate(x)]
         if len(found) > 1:
@@ -136,16 +92,27 @@ class LogFrameSearchStream(LogFrameStream):
 
     def search(self) -> Optional[LogFrame]:
         if self.sucess:
-            return self.data
+            result = self.data
+            # return self.data
         elif self._stopped:
-            return None
-        for _ in self:
-            if self.predicate(self.data.curr):
-                self.sucess = True
-                self._stopped = True
-                return self.data
+            result = None
+            # return None
         else:
-            return None
+            try:
+                result = next(frame for frame in self if self.predicate(frame.current))
+                self.sucess = True
+            except StopIteration:
+                result = None
+            self._stopped = True
+        return result
+        # return next(filter(lambda frame: self.predicate(frame.current), self))
+        # for _ in self:
+        #     if self.predicate(self.data.current):
+        #         self.sucess = True
+        #         self._stopped = True
+        #         return self.data
+        # else:
+        #     return None
 
 
 
